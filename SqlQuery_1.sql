d767301ede94cd7a77bde68bbd016a0c78d93d7b
Michael De Marco <105325093+mikedemarco@users.noreply.github.com>
2022-10-25 20:09:42 -0400

    Updated SQL

---
+++ SqlQuery_1.sql
@@ -0,0 +1,11 @@
+﻿-- 3. Select the Customer Name for the OrderID 10310
+SELECT Orders.OrderID, Customers.CustomerName
+FROM Orders
+INNER JOIN Customers ON Orders.CustomerID=Customers.CustomerID
+WHERE Orders.OrderID= '10310';
+
+-- 4. Select the address for the supplier of ProductID 40
+SELECT Products.SupplierID, Suppliers.SupplierID, Suppliers.Address
+FROM Products
+INNER JOIN Suppliers ON Products.SupplierID=Suppliers.SupplierID
+WHERE ProductID= '40';
